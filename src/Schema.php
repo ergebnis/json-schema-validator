cff7eaf7ccadc4dbb85ada36786313786791f67d
Andreas Möller <am@localheinz.com>
2021-02-02 12:28:19 +0100

    Enhancement: Implement Schema

---
+++ src/Schema.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2021 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/json-schema-validator
+ */
+
+namespace Ergebnis\Json\SchemaValidator;
+
+final class Schema
+{
+    private $decoded;
+
+    private function __construct(\stdClass $decoded)
+    {
+        $this->decoded = $decoded;
+    }
+
+    /**
+     * @throws \InvalidArgumentException
+     */
+    public static function fromJson(Json $json): self
+    {
+        $decoded = \json_decode($json->encoded());
+
+        if (!$decoded instanceof \stdClass) {
+            throw Exception\InvalidSchema::notAnObject();
+        }
+
+        return new self($decoded);
+    }
+
+    public function decoded(): \stdClass
+    {
+        return $this->decoded;
+    }
+}
---
+++ test/Unit/Exception/InvalidSchemaTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2021 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/json-schema-validator
+ */
+
+namespace Ergebnis\Json\SchemaValidator\Test\Unit\Exception;
+
+use Ergebnis\Json\SchemaValidator\Exception;
+use PHPUnit\Framework;
+
+/**
+ * @internal
+ *
+ * @covers \Ergebnis\Json\SchemaValidator\Exception\InvalidSchema
+ */
+final class InvalidSchemaTest extends Framework\TestCase
+{
+    public function testNotAnOjectReturnsException(): void
+    {
+        $exception = Exception\InvalidSchema::notAnObject();
+
+        self::assertSame('Value does not appear to be a valid JSON schema.', $exception->getMessage());
+    }
+}
